b6708358cb55bf4630091f58ea9f4951a957c8ca
Larry Hawelu <lhawelu@gmail.com>
2021-04-19 13:20:14 -0700

    Initial react routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
 import './App.css';
+import MarketPlace from './Pages/MarketPlace'
+import UserPage from './Pages/UserPage'
 
 
 class App extends React.Component {
@@ -10,7 +13,10 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        
+        <Switch>  
+          <Route exact path="/marketplace" component={MarketPlace}/>
+          <Route exact path="/users/:id" component={UserPage}/>
+        </Switch>
       </div>
     )
   }
