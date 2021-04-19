e14ba0f839c33de27f1f178b9e26ab09dae5cc91
Soo-Beom <soobeomkim77@gmail.com>
2021-04-19 15:14:49 -0700

    semantic changes

--- backend/app/models/purchase.rb
+++ backend/app/models/purchase.rb
@@ -1,6 +1,6 @@
 class Purchase < ActiveRecord::Base
     belongs_to :item
-    belongs_to :seller
-    belongs_to :purchaser
+    belongs_to :seller, :class_name => "User"
+    belongs_to :purchaser, :class_name => "User"
 
 end
