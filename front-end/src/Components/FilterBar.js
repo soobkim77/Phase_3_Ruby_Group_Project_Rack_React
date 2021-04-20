a7b44513d2f3f74ebf694b0319aeedae45f51332
Larry Hawelu <lhawelu@gmail.com>
2021-04-20 10:55:54 -0700

    finishing filters on marketplace

--- front-end/src/Components/FilterBar.js
+++ front-end/src/Components/FilterBar.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Dropdown } from 'semantic-ui-react'
 
 const categoryOptions = [
@@ -23,25 +24,60 @@ const categoryOptions = [
   }
 ]
 
-const FilterBar = (props) => {
+class FilterBar extends React.Component {
+
+  state={
+    input: ""
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  render() {
     return (
-        <div>
-            <h1>Filter</h1>
-            <div>
-              <Dropdown onChange={props.handleDropDown}
-                placeholder='Select a Category'
-                fluid
-                selection
-                options={categoryOptions}
-              />
-            </div>
-            <div>
-                User:
-                <input type="text" placeholder="search by username"></input>
-                <button>Search </button>
-            </div>
-        </div>
+      <div>
+          <h1>Filter</h1>
+          <div>
+            <Dropdown onChange={this.props.handleCategoryDropDown}
+              placeholder='Select a Category'
+              fluid
+              selection
+              options={categoryOptions}
+            />
+          </div>
+          <div>
+              User:
+              <input onInput={this.handleChange} type="text" placeholder="search by name"></input>
+              <button onClick={() => this.props.handleUserFilter(this.state.input)}>Search </button>
+          </div>
+      </div>
     ) 
+  }
+
 }
+
+// const FilterBar = (props) => {
+//     return (
+//         <div>
+//             <h1>Filter</h1>
+//             <div>
+//               <Dropdown onChange={props.handleDropDown}
+//                 placeholder='Select a Category'
+//                 fluid
+//                 selection
+//                 options={categoryOptions}
+//               />
+//             </div>
+//             <div>
+//                 User:
+//                 <input type="text" placeholder="search by username"></input>
+//                 <button>Search </button>
+//             </div>
+//         </div>
+//     ) 
+// }
 
 export default FilterBar
--- front-end/src/Components/ItemCard.js
+++ front-end/src/Components/ItemCard.js
@@ -1,7 +1,6 @@
 import { Card, Image } from "semantic-ui-react";
 
 const ItemCard = (props) => {
-  console.log(props.items)
     return (
       <Card raised>
         <Image src={props.item.imageUrl} />
