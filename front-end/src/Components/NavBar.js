326ecaa5ff34171422f58bdd47fdbd174e654a86
Larry Hawelu <lhawelu@gmail.com>
2021-04-20 15:06:00 -0700

    updating login flow

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -2,18 +2,19 @@ import { NavLink } from "react-router-dom";
 
 
 const NavBar = (props) => {
+  
+      return (
+        <div style={{ borderBottom: "2px solid black", paddingBottom: "10px", marginBottom: "12px" }}>
+            <NavLink style={{ marginRight: "10px" }} to={`/users/${props.user.id}`}>
+                My Page
+            </NavLink>
+            <NavLink style={{ marginRight: "10px" }} to="/marketplace">
+                Marketplace
+            </NavLink>
+        </div>
+        )
 
+    }
 
-  return (
-    <div style={{ borderBottom: "2px solid black", paddingBottom: "10px", marginBottom: "12px" }}>
-        <NavLink style={{ marginRight: "10px" }} to={`/users/${props.user.id}`}>
-            My Page
-        </NavLink>
-        <NavLink style={{ marginRight: "10px" }} to="/marketplace">
-            Marketplace
-        </NavLink>
-    </div>
-    );
-}
 
 export default NavBar
