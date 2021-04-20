7caa04c204d70e4704f9e6d700a1b7bf3678b2b2
Soo-Beom <soobeomkim77@gmail.com>
2021-04-20 15:05:46 -0700

    added random css 4 fun

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -190,7 +190,7 @@ class App extends React.Component {
           <LogIn log={this.state.login} user={this.state.user} handleUsernameChange={this.handleUsernameChange} handlePasswordChange={this.handlePasswordChange}  handleLogin={(e) => this.createUser(e)} />
           }
         </div>
-        <h1 className="ui header">Welcome to Jankazon</h1>
+        <h1 className="ui header welcome">Welcome to Jankazon</h1>
         <Switch>  
             <Route exact path="/marketplace" render={()=> {
               return <MarketPlace items={this.state.items} />
--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -5,14 +5,14 @@ const NavBar = (props) => {
 
 
   return (
-    <div style={{ borderBottom: "2px solid black", paddingBottom: "10px", marginBottom: "12px" }}>
+    <div style={{ borderBottom: "2px solid black", paddingBottom: "10px", marginBottom: "12px" }} className="navbar">
         <NavLink style={{ marginRight: "10px" }} to={`/users/${props.user.id}`}>
             My Page
         </NavLink>
         <NavLink style={{ marginRight: "10px" }} to="/marketplace">
             Marketplace
         </NavLink>
-        <button onClick={props.logout}>LogOut</button>
+        <button onClick={props.logout} className="logout">LogOut</button>
     </div>
     );
 }
