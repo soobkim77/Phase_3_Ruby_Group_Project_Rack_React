0b8cf881fda52f3567c2fda41169e51949df51d8
Soo-Beom <soobeomkim77@gmail.com>
2021-04-21 12:14:48 -0700

    fixed merge conflict

--- front-end/src/Components/ItemCard.js
+++ front-end/src/Components/ItemCard.js
@@ -2,54 +2,13 @@ import { Card, Image, Icon } from "semantic-ui-react";
 
 const ItemCard = (props) => {
     return (
-      // <Card raised onClick={(e) => props.view(e, props.item)}>
-      //   <Image src={props.item.imageUrl} />
-      //   <Card.Content>
-      //     <Card.Header>
-      //         {props.item.name}
-      //     </Card.Header>
-      //     <Card.Description>
-      //         Price: {props.item.price} <br />
-      //         Condition: {props.item.condition} <br />
-      //         Description: {props.item.description} <br />
-      //         Category: {props.item.category} <br />
-      //     </Card.Description>
-      //     {props.myPage ? <button 
-      //               className="userRmvBtn" 
-      //               id={props.item.id} 
-      //               onClick={()=>props.remove(props.item)}>x</button> : null}
-      //   </Card.Content>
-      // </Card>
       <Card raised onClick={(e) => props.view(e, props.item)}>
         <Image src={props.item.imageUrl} />
         <Card.Content>
           <Card.Header>
               {props.item.name}
           </Card.Header>
-        </Card.Content>
-        <Card.Content>
           <Card.Description>
-<<<<<<< HEAD
-            <strong>About this item</strong><br />
-            {props.item.description}
-          </Card.Description> <br />   
-        <Card.Meta >
-          <strong>Category:</strong> {props.item.category}<br />  
-          <strong>Condition:</strong> {props.item.condition}
-        </Card.Meta>
-        </Card.Content>
-        <Card.Content extra>
-          <a>
-            <Icon name='dollar' />
-            {props.item.price} 
-          </a>
-          {props.myPage ? 
-            <a className="ui right floated">
-              <Icon name='delete' id={props.item.id} onClick={()=>props.remove(props.item)}/>
-            </a>
-          : 
-            null}
-=======
               Price: {props.item.price} <br />
               Condition: {props.item.condition} <br />
               Description: {props.item.description} <br />
@@ -58,10 +17,10 @@ const ItemCard = (props) => {
           {props.myPage ? <button 
                     className="userRmvBtn" 
                     id={props.item.id} 
-                    onClick={(e)=>props.remove(e,props.item)}>x</button> : null}
->>>>>>> soobs
+                    onClick={()=>props.remove(props.item)}>x</button> : null}
         </Card.Content>
       </Card>
+     
     )
 }
 
