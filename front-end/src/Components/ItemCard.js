00f3f76728f37877436131f7c26367557d0f6ce2
Larry Hawelu <lhawelu@gmail.com>
2021-04-22 09:58:06 -0700

    fixing item card formatting

--- front-end/src/Components/ItemCard.js
+++ front-end/src/Components/ItemCard.js
@@ -1,29 +1,38 @@
-import { Card, Image, Icon } from "semantic-ui-react";
+import { Card, Image, Icon, Button } from "semantic-ui-react";
 
 const ItemCard = (props) => {
     return (
       <Card raised onClick={(e) => props.view(e, props.item)}>
-        <Image src={props.item.imageUrl} />
+        <Image src={props.item.imageUrl} />   
         <Card.Content>
           <Card.Header>
-              {props.item.name}
+          {props.item.name}
           </Card.Header>
           <Card.Description>
-              Price: {props.item.price} <br />
-              Condition: {props.item.condition} <br />
-              Description: {props.item.description} <br />
-              Category: {props.item.category} <br />
-          </Card.Description>
-          {props.myPage ? <button 
-                    className="userRmvBtn" 
-                    id={props.item.id} 
-                    onClick={()=>props.remove(props.item)}>x</button> :
-                    <button 
-                    id={props.item.id} 
-                    onClick={(e)=>props.buy(e, props.item)}>Buy Item</button>}
+            <strong>About this item</strong><br />
+            {props.item.description}
+          </Card.Description> <br />   
+          <Card.Meta >
+            <strong>Category:</strong> {props.item.category}<br />  
+            <strong>Condition:</strong> {props.item.condition}
+          </Card.Meta>
+        </Card.Content>
+        <Card.Content extra>
+          <span>
+            <Icon name='dollar' />
+            {props.item.price} 
+          </span>
+          {props.myPage ? 
+            <span className="ui right floated">
+              <Icon name='delete' id={props.item.id} onClick={(e)=>props.remove(e, props.item)}/>
+            </span>
+          : 
+            <span className="ui right floated">
+              <Button size="mini" id={props.item.id} onClick={(e)=>props.buy(e, props.item) }>Buy Item</Button>
+            </span>
+        }
         </Card.Content>
       </Card>
-     
     )
 }
 
